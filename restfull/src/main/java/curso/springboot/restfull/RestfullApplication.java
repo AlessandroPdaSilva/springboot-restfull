a79851f6b7083ed5f51cff72289285db311f85f0
Alessandro <alessandropds55@gmail.com>
2022-09-07 13:43:00 -0300

    Aprendendo a usar CORS ou CrossOrigin

--- restfull/src/main/java/curso/springboot/restfull/RestfullApplication.java
+++ restfull/src/main/java/curso/springboot/restfull/RestfullApplication.java
@@ -3,6 +3,9 @@ package curso.springboot.restfull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 	/*
@@ -14,10 +17,24 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 		@RestController
 		@EnableAutoConfiguration
 	 */
-public class RestfullApplication {
+public class RestfullApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullApplication.class, args);
 	}
-
+	
+	
+	// Exemplo de CrossOrigin global
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		/*
+		 
+			registry.addMapping("/usuario")
+			.allowedMethods("POST","DELETE")
+			.allowedOrigins("www.meusite.com.br");
+		
+		*/
+	}
+	
 }
--- restfull/src/main/java/curso/springboot/restfull/controller/UsuarioController.java
+++ restfull/src/main/java/curso/springboot/restfull/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ public class UsuarioController {
 	private UsuarioRepository usuarioRepository;
 
 	// LISTAR USUARIOS
+	@CrossOrigin(origins = "*")
 	@GetMapping(value = "")
 	public ResponseEntity listarUsuario(){
 		
@@ -48,6 +50,7 @@ public class UsuarioController {
 	}
 	
 	// SALVAR USUARIO
+	@CrossOrigin(origins = "localhost:8080")
     @PostMapping(value = "")
     public ResponseEntity<?> salvarUsuario(@RequestBody Usuario usuario){
     	
