ad462a26bd3b37b946e78fc981cde9e827b4d30c
alessandro.silva <alessandro.silva@ESTACAO.SETUR.DF.GOV.BR>
2022-09-09 17:16:38 -0300

    Tratamento de exceções da API

---
+++ restfull/src/main/java/curso/springboot/restfull/ObjErro.java
@@ -0,0 +1,21 @@
+package curso.springboot.restfull;
+
+public class ObjErro {
+	private String error;
+	private String code;
+	
+	// GET E SET
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+}
